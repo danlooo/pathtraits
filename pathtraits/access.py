165ff056cc6065c3f3196b56f8c55b5e8acd1f97
danlooo <dloos@bgc-jena.mpg.de>
2025-12-19 09:54:07 +0100

    Fix lint errors in access module

--- pathtraits/access.py
+++ pathtraits/access.py
@@ -1,4 +1,8 @@
-import os
+"""
+Module for accessing path traits from the database.
+"""
+
+import sys
 import logging
 import yaml
 from pathtraits.db import TraitsDB
@@ -7,6 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 def get(path, db_path, verbose):
+    """
+    Docstring for get
+
+    :param path: Description
+    :param db_path: Description
+    :param verbose: Description
+    """
     if verbose:
         logging.basicConfig(level=logging.DEBUG)
 
@@ -15,5 +26,5 @@ def get(path, db_path, verbose):
     if len(res) > 0:
         print(yaml.safe_dump(res))
     else:
-        logger.error(f"No traits found for path {path} in database {db_path}")
-        exit(1)
+        logger.error("No traits found for path %s in database %s", path, db_path)
+        sys.exit(1)
